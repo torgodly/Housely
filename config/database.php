da07b262e015b89e16c7d22820faa76a9b59a151
torgodly <torgodly@gmail.com>
2023-05-23 19:13:32 +0200

    fix database and seed an admin

--- config/database.php
+++ config/database.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use Illuminate\Support\Str;
@@ -57,7 +58,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => null,
+            'engine' => 'InnoDB',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
