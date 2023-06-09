c267bcc209f39110d9be8692b419ec1a1d0b4350
torgodly <torgodly@gmail.com>
2023-06-09 23:58:12 +0200

    create show funnction

--- app/Http/Controllers/EstateController.php
+++ app/Http/Controllers/EstateController.php
@@ -15,28 +15,11 @@ class EstateController extends Controller
         return view('Estate.create', compact('utilities'));
     }
 
-    public function store(Request $request)
+//show
+    public function show(Estate $estate)
     {
-
-        dd($request->all());
-        // validate the form input fields
-//        $validatedData = $request->validate([
-//            'name' => 'required|string|max:255',
-//            // add other fields here as needed
-//        ]);
-
-        // create a new property
-        $property = Estate::find(1);
-
-        // sync the utility quantities with the property
-        $utilities = collect($request->utilities)->filter(function ($utility) {
-            return isset($utility['quantity']) && $utility['quantity'] > 0;
-        })->mapWithKeys(function ($utility) {
-            return [$utility['id'] => ['quantity' => $utility['quantity']]];
-        });
-        $property->utilities()->sync($utilities);
-
-        return redirect()->back()->with('success', 'Estate created successfully!');
+//        dd($estate);
+        return view('Estate.show', compact('estate'));
     }
 
 }
