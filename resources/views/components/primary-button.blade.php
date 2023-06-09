54ad3d39a9a63449ad3f77147f332d3f92d7b705
torgodly <torgodly@gmail.com>
2023-06-09 23:56:35 +0200

    edting primary buttton

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,5 +1,6 @@
 @props(['disabled' => false])
 
-<button {{ $disabled ? 'disabled' : '' }}  {{ $attributes->merge(['type' => 'submit', 'class' => 'disabled:opacity-25 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+<button {{ $disabled ? 'disabled' : '' }}  {{ $attributes->merge(['type' => 'submit', 'class' => 'disabled:opacity-25 inline-flex items-center px-4 py-2 bg-primary border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-primary focus:bg-primary active:bg-primary focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
+
