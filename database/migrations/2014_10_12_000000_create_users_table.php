03c619c8c82515118c2c4de8f91930451c863bc7
torgodly <torgodly@gmail.com>
2023-07-07 17:51:49 +0200

    adding role to user database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum("role", ["admin", "user"])->default("user");
+            $table->string('phone_number');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
