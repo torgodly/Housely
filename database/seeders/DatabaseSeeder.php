3c2da5e3db270f0ea7f4312c2d259460126cce70
torgodly <torgodly@gmail.com>
2023-12-30 13:39:50 +0200

    remove seed exampt users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Estate;
-use App\Models\Utility;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,22 +12,22 @@ class DatabaseSeeder extends Seeder
 //     */
     public function run(): void
     {
-         \App\Models\User::factory(10)->create();
-        Utility::factory(100)->create();
-
-//        create 10 estate and attach 5 utilities to each one with random quantity
-        Estate::factory(100)->create()->each(function ($estate) {
-            $utilities = Utility::all()->random(5)->pluck('id');
-            $estate->utilities()->attach($utilities, ['quantity' => rand(1, 8)]);
-            $images = \App\Models\Image::factory(5)->create(
-                [
-                    'estate_id' => $estate->id,
-
-                ]
-            );
-        });
-//        create 10 orders
-        \App\Models\Order::factory(500)->create();
+//         \App\Models\User::factory(10)->create();
+//        Utility::factory(100)->create();
+//
+////        create 10 estate and attach 5 utilities to each one with random quantity
+//        Estate::factory(100)->create()->each(function ($estate) {
+//            $utilities = Utility::all()->random(5)->pluck('id');
+//            $estate->utilities()->attach($utilities, ['quantity' => rand(1, 8)]);
+//            $images = \App\Models\Image::factory(5)->create(
+//                [
+//                    'estate_id' => $estate->id,
+//
+//                ]
+//            );
+//        });
+////        create 10 orders
+//        \App\Models\Order::factory(500)->create();
 
 
         \App\Models\User::factory()->create([
